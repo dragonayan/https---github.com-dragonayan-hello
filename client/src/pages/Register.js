7073c505856fab3cc0b35ca2c23788cf9c8876c5
dragonayan <94789365+dragonayan@users.noreply.github.com>
2023-07-09 16:00:21 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -5,15 +5,17 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { hideLoading, showLoading } from "../redux/alertsSlice";
-
+import { useState } from "react";
 function Register() {
-  const dispatch = useDispatch();
+
   const navigate = useNavigate();
+  const[loading,setloading] =useState(false);
   const onFinish = async (values) => {
     try {
-      dispatch(showLoading());
+      
+      setloading(true);
       const response = await axios.post("/api/user/register", values);
-      dispatch(hideLoading());
+     setloading(false)
       if (response.data.success) {
         toast.success(response.data.message);
         navigate("/login");
@@ -21,15 +23,26 @@ function Register() {
         toast.error(response.data.message);
       }
     } catch (error) {
-      dispatch(hideLoading());
+   setloading(false)
       toast.error("Something went wrong");
     }
   };
 
   return (
-    <div className="authentication">
+    <div className="authentication" style={{ backgroundImage: "url('https://ik.imagekit.io/wnn5dux89/i1.jpg?updatedAt=1688845031574')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}>
+      <div
+        className="background-overlay"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          background: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))",
+        }}
+      ></div>
       <div className="authentication-form card p-3">
-        <h1 className="card-title">Nice To Meet U</h1>
+        <h1 className="card-title">Get Started</h1>
         <Form layout="vertical" onFinish={onFinish}>
           <Form.Item label="Name" name="name">
             <Input placeholder="Name" />
