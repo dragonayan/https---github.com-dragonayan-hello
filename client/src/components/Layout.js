1b3bd60e60cb7b3db10e698ec2592e55bc1bf769
dragonayan <94789365+dragonayan@users.noreply.github.com>
2023-07-09 17:11:58 +0530

    Update Layout.js

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,6 +3,7 @@ import "../layout.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Badge } from "antd";
+import { Ri24HoursFill } from "react-icons/ri";
 
 function Layout({ children }) {
   const [collapsed, setCollapsed] = useState(false);
@@ -62,21 +63,22 @@ function Layout({ children }) {
       path: "/admin/doctorslist",
       icon: "ri-user-star-line",
     },
-    {
-      name: "Profile",
-      path: "/profile",
-      icon: "ri-user-line",
-    },
   ];
 
   const menuToBeRendered = user?.isAdmin ? adminMenu : user?.isDoctor ? doctorMenu : userMenu;
   const role = user?.isAdmin ? "Admin" : user?.isDoctor ? "Doctor" : "User";
+
   return (
     <div className="main">
       <div className="d-flex layout">
-        <div className="sidebar">
+        {/* sidebar designing */}
+        <div className="sidebar" style={{ backgroundColor: "  #85929e   " }}>
           <div className="sidebar-header">
-            <h1 className="logo">SH</h1>
+            <Link to="/">
+              <h1 className="logo">
+                <Ri24HoursFill />
+              </h1>
+            </Link>
             <h1 className="role">{role}</h1>
           </div>
 
@@ -85,9 +87,8 @@ function Layout({ children }) {
               const isActive = location.pathname === menu.path;
               return (
                 <div
-                  className={`d-flex menu-item ${
-                    isActive && "active-menu-item"
-                  }`}
+                  className={`d-flex menu-item ${isActive && "active-menu-item"}`}
+
                 >
                   <i className={menu.icon}></i>
                   {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
@@ -129,7 +130,7 @@ function Layout({ children }) {
                 <i className="ri-notification-line header-action-icon px-3"></i>
               </Badge>
 
-              <Link className="anchor mx-2" to="/profile">
+              <Link className="anchor mx-2 anchor-circle" to="/">
                 {user?.name}
               </Link>
             </div>
@@ -138,6 +139,22 @@ function Layout({ children }) {
           <div className="body">{children}</div>
         </div>
       </div>
+      <style>
+        {`
+          body {
+            background-color: #333;
+            transition: background-color 0.3s ease;
+          },
+
+          .anchor-circle {
+            display: inline-block;
+            border: 2px solid blue;
+            border-radius: 30%;
+            padding: 8px;
+          },
+                   
+        `}
+      </style>
     </div>
   );
 }
