25703e9a4d31cea60c02316683675a160e60985e
dragonayan <94789365+dragonayan@users.noreply.github.com>
2023-07-09 16:00:03 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,22 @@
 import { Button, Form, Input } from "antd";
 import React from "react";
 import toast from "react-hot-toast";
-import { useSelector, useDispatch } from "react-redux";
+import {useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { hideLoading, showLoading } from "../redux/alertsSlice";
-
+import { useState } from "react";
 function Login() {
-  const dispatch = useDispatch();
+  const[loading,setloading] =useState(false);
+  
   const navigate = useNavigate();
   const onFinish = async (values) => {
     try {
-      dispatch(showLoading());
+      setloading(true);
+      
       const response = await axios.post("/api/user/login", values);
-      dispatch(hideLoading());
+   
+      setloading(false);
       if (response.data.success) {
         toast.success(response.data.message);
         localStorage.setItem("token", response.data.data);
@@ -22,13 +25,24 @@ function Login() {
         toast.error(response.data.message);
       }
     } catch (error) {
-      dispatch(hideLoading());
+      setloading(false)
       toast.error("Something went wrong");
     }
   };
 
   return (
-    <div className="authentication">
+    <div className="authentication" style={{ backgroundImage: "url('https://ik.imagekit.io/wnn5dux89/i1.jpg?updatedAt=1688845031574')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}>
+      <div
+        className="background-overlay"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          background: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))",
+        }}
+      ></div>
       <div className="authentication-form card p-3">
         <h1 className="card-title">Welcome Back</h1>
         <Form layout="vertical" onFinish={onFinish}>
@@ -38,6 +52,7 @@ function Login() {
           <Form.Item label="Password" name="password">
             <Input placeholder="Password" type="password" />
           </Form.Item>
+          
 
           
           <Button className="primary-button my-2 full-width-button" htmlType="submit">
